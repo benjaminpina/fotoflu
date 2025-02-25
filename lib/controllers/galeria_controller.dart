48066eef92f322b8f341b7415329ccabb3c1e9a0
Benjamín Piña Altamirano <benjamin.pina@gmail.com>
2025-02-25 12:44:04 -0600

    instalado photo_view

--- lib/controllers/galeria_controller.dart
+++ lib/controllers/galeria_controller.dart
@@ -1,3 +1,22 @@
+import 'dart:io';
 import 'package:get/get.dart';
 
-class GaleriaController extends GetxController {}
+class GaleriaController extends GetxController {
+  var images = <File>[].obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    loadImages();
+  }
+
+  void loadImages() async {
+    final imageDir = Directory(
+      '/home/benjamin/Imágenes/ninfas/Seleccion/achicadas',
+    );
+    if (await imageDir.exists()) {
+      final imageFiles = imageDir.listSync().whereType<File>().toList();
+      images.addAll(imageFiles);
+    }
+  }
+}
--- lib/ui/widgets/galeria.dart
+++ lib/ui/widgets/galeria.dart
@@ -1,14 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:photo_view/photo_view.dart';
+import 'package:photo_view/photo_view_gallery.dart';
+import 'package:fotoflu/controllers/galeria_controller.dart';
 
 class Galeria extends StatelessWidget {
-  const Galeria({super.key});
+  Galeria({super.key});
+
+  final controller = Get.find<GaleriaController>();
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       width: double.infinity,
       height: double.infinity,
-      child: Placeholder(child: const Text('Galería')),
+      child: Obx(() {
+        if (controller.images.isEmpty) {
+          return Center(child: Text('No hay imágenes disponibles.'));
+        } else {
+          return PhotoViewGallery.builder(
+            itemCount: controller.images.length,
+            builder: (context, index) {
+              return PhotoViewGalleryPageOptions(
+                imageProvider: FileImage(controller.images[index]),
+                minScale: PhotoViewComputedScale.contained,
+                maxScale: PhotoViewComputedScale.covered * 2,
+              );
+            },
+            scrollPhysics: BouncingScrollPhysics(),
+            backgroundDecoration: BoxDecoration(color: Colors.black),
+            pageController: PageController(),
+          );
+        }
+      }),
     );
   }
 }
