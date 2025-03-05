4e9d749d9192f79836065a3a21f32f2cfa7eea99
Benjamín Piña Altamirano <benjamin.pina@gmail.com>
2025-03-05 17:48:19 -0600

    función para obtener la lista de archivos de un directorio

--- lib/services/files.dart
+++ lib/services/files.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
+
 Future<Directory> _createFolder(String folderName) async {
   final folder = Directory(folderName);
 
@@ -21,4 +23,47 @@ Future<String?> crearEstrcutura(String base, List<String> dirs) async {
   }
 
   return null;
+}
+
+Future<String?> copyFile(
+  String sourcePath,
+  String newPath, {
+  bool preserveOriginal = true,
+}) async {
+  try {
+    final sourceFile = File(sourcePath);
+    await sourceFile.copy(newPath);
+  } catch (e) {
+    return 'Error al copiar el archivo: $e';
+  }
+
+  if (!preserveOriginal) {
+    try {
+      await File(sourcePath).delete();
+    } catch (e) {
+      return 'Error al eliminar el archivo original: $e';
+    }
+  }
+
+  return null;
+}
+
+Future<List<String>> filesByExt(String parentDir, String ext) async {
+  final dir = Directory(parentDir);
+  List<String> fileList = [];
+
+  try {
+    final entidades = dir.list();
+    await for (FileSystemEntity entidad in entidades) {
+      if (entidad is File && entidad.path.endsWith(ext)) {
+        fileList.add(entidad.path);
+      }
+    }
+  } catch (e) {
+    if (kDebugMode) {
+      print('Error al listar archivos: $e');
+    }
+  }
+
+  return fileList;
 }
