8d152d00e6e15cbca8b4b69dd5e4994fe0ef7a25
Benjamín Piña Altamirano <benjamin.pina@gmail.com>
2025-03-05 17:04:59 -0600

    método para crear estructura de directorios

--- lib/controllers/panel_lateral_controller.dart
+++ lib/controllers/panel_lateral_controller.dart
@@ -1,3 +1,28 @@
 import 'package:get/get.dart';
+import "package:fotoflu/services/files.dart";
+import 'package:fotoflu/repositories/destino_repository.dart';
+import 'package:fotoflu/controllers/storage_controller.dart';
 
-class PanelLateralController extends GetxController {}
+class PanelLateralController extends GetxController {
+  final storage = Get.find<StorageController>();
+  final destinos = Get.find<DestinoRepository>();
+
+  void crearEstructura() async {
+    final base = storage.dir;
+    final List<String> dirs = [];
+
+    for (final destino in destinos.destinos) {
+      if (destino.nombre != null && destino.nombre!.isNotEmpty) {
+        dirs.add(destino.nombre!);
+      }
+    }
+
+    crearEstrcutura(base, dirs).then((value) {
+      if (value != null) {
+        Get.snackbar('Error', value);
+      } else {
+        Get.snackbar('Info', 'Estructura de directorios creada');
+      }
+    });
+  }
+}
