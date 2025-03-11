c4be56b4490ec146d1a6a591dee20a5d8f225fbb
Benjamín Piña Altamirano <benjamin.pina@gmail.com>
2025-03-10 20:10:43 -0600

    creado observavle en HomeController con estatus de la aplicación

--- lib/controllers/home_controller.dart
+++ lib/controllers/home_controller.dart
@@ -1,8 +1,15 @@
 import 'package:get/get.dart';
 
+enum AppState { inicial, explorando, sinImagenes }
+
 class HomeController extends GetxController {
   final cambioIdSelected = Rxn<int>();
   final contCambios = 0.obs;
   final contSeleccionadas = 0.obs;
   final contParaBorrar = 0.obs;
+  final appState = AppState.inicial.obs;
+
+  void setAppState(AppState value) {
+    appState.value = value;
+  }
 }
